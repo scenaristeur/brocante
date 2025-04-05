9fb06200263bd739372aeec827ac680a08e7446a
scenaristeur <scenaristeur@gmail.com>
2025-04-05 22:13:25 +0200

    pwa

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,52 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
+import { resolve, dirname } from 'node:path'
+const base = process.env.NODE_ENV === 'production' ? '/brocante/' : '/'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: base,
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Brocante',
+        includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
--- vitest.config.js
+++ vitest.config.js
@@ -1,59 +1,10 @@
 import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
-import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
 import viteConfig from './vite.config'
-const base = process.env.NODE_ENV === 'production' ? '/brocante/' : '/'
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
-    base: base,
-    plugins: [
-      vue(),
-      VitePWA({
-        registerType: 'autoUpdate',
-        manifest: {
-          name: 'Brocante',
-          includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
-          theme_color: '#ffffff',
-          icons: [
-            {
-              src: 'pwa-64x64.png',
-              sizes: '64x64',
-              type: 'image/png',
-            },
-            {
-              src: 'pwa-192x192.png',
-              sizes: '192x192',
-              type: 'image/png',
-            },
-            {
-              src: 'pwa-512x512.png',
-              sizes: '512x512',
-              type: 'image/png',
-              purpose: 'any',
-            },
-            {
-              src: 'maskable-icon-512x512.png',
-              sizes: '512x512',
-              type: 'image/png',
-              purpose: 'maskable',
-            },
-          ],
-        },
-      }),
-      // VueI18nPlugin({
-      //   /* options */
-      //   // locale messages resource pre-compile option
-      //   include: resolve(dirname(fileURLToPath(import.meta.url)), './path/to/src/locales/**'),
-      // }),
-    ],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
-      },
-    },
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
